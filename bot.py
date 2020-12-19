c65c29e56168e5637b6dc802cd12d29737a4dd42
huang12zheng <huang12zheng@163.com>
2020-12-19 13:22:21 +0800

    init

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ app = nonebot.get_asgi()
 driver = nonebot.get_driver()
 driver.register_adapter("cqhttp", CQHTTPBot)
 nonebot.load_builtin_plugins()
-nonebot.load_plugins("remind/plugins")
+nonebot.load_plugins("src/plugins")
 
 # Modify some config / config depends on loaded configs
 # 
