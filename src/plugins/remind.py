2a420ca0ef1ff4e7130388d3d526219ffbeca8dc
huang12zheng <huang12zheng@163.com>
2020-12-20 13:03:20 +0800

    add 统计信息

--- src/plugins/remind.py
+++ src/plugins/remind.py
@@ -6,6 +6,8 @@ from redis.connection import ConnectionPool
 
 import utils.bot_io as botIO
 import random
+import datetime
+from collections import Counter
 
 # port:int = Bot.
 driver = get_driver()
@@ -13,42 +15,117 @@ pool = ConnectionPool(host='host.docker.internal', port=driver.config.redis_port
 r = Redis(host='host.docker.internal', port=driver.config.redis_port, decode_responses=True)
 
 mark = on_command("jc", aliases={"记恨","标记","记仇","jh","bj"})
-search=on_command("search", aliases={"查询","查找"})
-
+search=on_command("search", aliases={"查询","查找","s"})
+sw=on_command("sw", aliases={"swho"})
+mergeByGenfun=botIO.mergeByGenfun
 class BJ:
-    victim:str
-    # assassin:str
-    assassins:[str]
-    victimValue:int
-    # assassinValue:int
-    assassinValues:[int]
     r:Redis
+    # 受害者
+    victim:str
+    # 伤害者们
+    assassins:[str]
+    # 本人受害数
+    victimValue:int
+    # 伤害者们的伤害数,
+    # "assassinValue:{assassin}"为单个伤害者的伤害数
+    assassinValues:[int]
+    # 伤害者们对victim的伤害数
+    assassinVictimValues:[int]
+    # 当日本人受害数
+    curVictimValue:int
+    # 当日伤害者们的伤害数
+    # "curAssassinValue:{date}:{assassin}"
+    curAssassinValues:[int]
+    # 当日伤害者们的伤害victim数
+    # "curAssassinVictimValue:{date}:{self.victim}:{assassin}"
+    curAssassinVictimValues:[int]
+    isBJCountGt5:bool
 
     def __init__(self,victim,assassinsStr,r:Redis):
-        self.victim=str(victim)
-        # self.assassin=str(assassinsStr)
-        self.assassins = str.split(assassinsStr)
         self.r = r
+        
+        self.victim=str(victim)
+        date:str = datetime.datetime.now().strftime('%y%m%d')
+
+        
+        _assassins = str.split(assassinsStr) #伤害者列表
+        assassinCount = Counter(_assassins) # xx.value is detaValue
+        self.assassins = list(assassinCount)
+        count = assassinCount.most_common(1)[0][1]
+        if count>5:
+            self.isBJCountGt5=True
+            return
+        else:
+            self.isBJCountGt5=False
+
         # victim受伤次数+=伤害者数 # 同名重复
-        self.victimValue = botIO.getInt(r,f"victimCount:{self.victim}")+len(self.assassins)
-        assassinsKey:[str] = [ f"assassinCount:{assassin}" for assassin in self.assassins ]
+        self.victimValue = botIO.getInt(r,f"victimValue:{self.victim}")+len(_assassins)
+        
+        self.curVictimValue=botIO.getInt(r,f"curVictimValue:{date}:{self.victim}")+len(_assassins)
+        
         # assassin伤害他人次数
-        self.assassinValues = [ 1 if value is None else int(value)+1 for value in r.mget(assassinsKey)]
-        # for assassin in assassins:
-        #   assassinValues[assassin] = botIO.getInt(r,f"assassin:{assassin}")+1
+        self.assassinValues = mergeByGenfun(r,
+            self.assassins, #detaKeysFrom #assassinCount.keys
+            lambda assassin: f"assassinValue:{assassin}",
+            list(assassinCount.values()) #detaValue # assassinCount.values
+        )
+
+        self.assassinVictimValues = mergeByGenfun(r,
+            self.assassins, #detaKeysFrom
+            lambda assassin: f"assassinVictimValue:{self.victim}:{assassin}",
+            list(assassinCount.values()) # #detaValue
+        )
+        
+        # 当日assassins伤害victim次数
+        self.curAssassinVictimValues = mergeByGenfun(r,
+            self.assassins, #detaKeysFrom
+            lambda assassin: f"curAssassinVictimValue:{date}:{self.victim}:{assassin}",
+            list(assassinCount.values()) # #detaValue
+        )
+        
+        # 当日assassins伤害他人次数
+        self.curAssassinValues = mergeByGenfun(r,
+            self.assassins, #detaKeysFrom
+            lambda assassin: f"curAssassinValue:{date}:{assassin}",
+            list(assassinCount.values()) # #detaValue
+        )
         
         print("successed")
     def save(self):
-        r.set("victimCount:"  +self.victim,   self.victimValue)
+        # 日期
+        date:str = datetime.datetime.now().strftime('%y%m%d')
+        r.sadd(f"assassins:{self.victim}",*set(self.assassins))
+        r.set(f"victimValue:"  +self.victim,   self.victimValue)
+        r.set(f"curVictimValue:{date}:"+self.victim,   self.curVictimValue)
+        
         r.mset(dict(
             zip(
-                [ f"assassinCount:{assassin}" for assassin in self.assassins ],
+                [ f"assassinValue:{assassin}" for assassin in self.assassins ],
                 self.assassinValues
             ))
         )
-        r.sadd(f"assassins:{self.victim}",*set(self.assassins))
+        r.mset(dict(
+            zip(
+                [ f"curAssassinValue:{date}:{assassin}" for assassin in self.assassins ],
+                self.curAssassinValues
+            ))
+        )
+
+        r.mset(dict(
+            zip(
+                [ f"assassinVictimValue:{self.victim}:{assassin}" for assassin in self.assassins ],
+                self.assassinVictimValues
+            ))
+        )
+
+        r.mset(dict(
+            zip(
+                [ f"curAssassinVictimValue:{date}:{self.victim}:{assassin}" for assassin in self.assassins ],
+                self.curAssassinVictimValues
+            ))
+        )
         
-        # r.sadd(f"victims:{self.assassin}",self.victim) 
+        # assassin 的受害者们是
         for assassin in self.assassins:
             r.sadd(f"victims:{assassin}", self.victim)
 
@@ -62,26 +139,73 @@ async def handle_mark(bot: Bot, event: Event, state: dict):
 
 @mark.got("BJ", prompt="你想标记谁?")
 async def handle_got_bj(bot: Bot, event: Event, state: dict):
-
     bj:BJ = state["BJ"]
-    # mes:str = f"{bj.assassin}的被标记次数:{bj.assassinValue}"
-    ids = [ id for id in range(len(bj.assassins))]
-    mes:str = '\n'.join([
-        f"{bj.assassins[id]}的被标记次数:{bj.assassinValues[id]}" for 
-            id in range(len(bj.assassins))
-        ])
-    await bot.send(event,mes)
-    bj.save()
-    flag = random.random() 
-    if flag > 0.87: await handle_search(bot,event,state) 
+    if bj.isBJCountGt5 :
+        await bot.send(event,"标记个数过多") # TODO
+    else:
+        ids = [ id for id in range(len(bj.assassins))]
+        mes:str = '\n'.join([
+            f"{bj.assassins[id]}的被标记次数:{bj.assassinValues[id]}" for 
+                id in range(len(bj.assassins))
+            ])
+        await bot.send(event,mes)
+        bj.save()
+        flag = random.random() 
+        if flag > 0.87: await handle_search(bot,event,state) 
 
 
 @search.handle()
 async def handle_search(bot: Bot, event: Event, state: dict):
+    date:str = datetime.datetime.now().strftime('%y%m%d')
     who:str = str(event.user_id)
-    victimValue=r.get("victimCount:"+who)
-    assassins=r.smembers("assassins:"+who)
-    doValue=r.get("assassinCount:"+who)
 
-    mes:str = f"{who}的情况是:\n受到伤害次数:{victimValue} \n伤害者: {assassins}\n被标记次数: {doValue}"
+    victimValue=r.get("victimValue:"+who)
+    _assassins=r.smembers("assassins:"+who)
+    assassins=list(_assassins)
+    doValue=r.get("assassinValue:"+who)
+    curAssassinValue=r.get(f"curAssassinValue:{date}:{who}")
+    curMostAssassinId=botIO.findIdWhere(
+        r.mget([ f"curAssassinVictimValue:{date}:{who}:{assassin}" for assassin in assassins]),
+        max
+    )
+    mostAssassinId=botIO.findIdWhere(
+        r.mget([ f"assassinVictimValue:{who}:{assassin}" for assassin in assassins]),
+        max
+    )
+    
+    curMostAssassin=""
+
+    mes:str = f"""{who}的情况是:
+        受到伤害次数:{victimValue}
+        伤害者: {assassins}
+        被标记次数: {doValue}
+        当日被标记次数: {curAssassinValue}
+        谁最伤害你:{None if mostAssassinId == None else assassins[mostAssassinId]}
+        你当日的最大标记者是:{None if curMostAssassinId == None else assassins[curMostAssassinId]}
+    """
     await bot.send(event,mes)
+@sw.handle()
+async def handle_sw(bot: Bot, event: Event, state: dict):
+    argsStr = str(event.message).strip()  # 首次发送命令时跟随的参数，例：/天气 上海，则args为上海
+    if argsStr:
+        args = argsStr.split()
+        if len(args)>1 :
+            await bot.send(event,"参数过多")
+        elif len(args)==0:
+            # await bot.send(event,"参数过多")
+            await handle_search(bot,event,state)
+        else:
+            victim = argsStr
+            date:str = datetime.datetime.now().strftime('%y%m%d')
+            curVictimValue=botIO.getInt(r,f"curVictimValue:{date}:{victim}")
+            curAssassinValue=r.get(f"curAssassinValue:{date}:{victim}")
+            mes:str = f"""{victim}
+            当日被伤害次数:{curVictimValue}
+            当日被标记次数: {curAssassinValue}
+            """
+            await bot.send(event,mes)
+            # 当日被标记次数: {curAssassinValue}\n
+        # state["BJ"] = BJ(event.user_id,event.plain_text,r)
+
+
+# def checkBJCount(maxCount:int=5):
--- utils/bot_io.py
+++ utils/bot_io.py
@@ -27,3 +27,23 @@ def getInt(r: Redis,key: str):
 async def sendlock(m:Matcher, r:Redis,key:str,value,func=lambda v: str(v) ): 
     await m.send(func(value))
     r.set(key,value)
+
+def merge(keylen,l1,l2,func=lambda a,b: a+b):
+    return [ func(l1[id],l2[id]) for id in range(keylen) ]
+def curMset():pass
+def mergeBykeys(r:Redis,detaKeys:[str],detaValue,func=lambda a,b: a+b): 
+    # oldValue = [ 0 if value is None else int(value) for value in r.mget(detaKeys)]
+    oldValue = getNoNone(r.mget(detaKeys))
+    return merge(len(detaKeys),oldValue,detaValue,func)
+def mergeByGenfun(r:Redis,keys:[str],genfunc,detaValue,func=lambda a,b: a+b): 
+    detaKeys=[ genfunc(key) for key in keys ]
+    return mergeBykeys(r,detaKeys,detaValue,func)
+def findIdWhere(l,func):
+    _l=getNoNone(l)
+    # if (len(l)>0):
+    #     return _l.index(func(_l))
+    # else:
+    #     return
+    return None if (len(l)==0) else _l.index(func(_l))
+def getNoNone(l,func=int):
+    return [ 0 if value is None else func(value) for value in l]
